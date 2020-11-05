c525f7749f528cdc083a579027accdd9b520a024
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-11-05 13:13:22 +0700

    create admin dashboard and API

---
+++ routes/admin.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const adminController = require("../controllers/adminController");
+const { upload, uploadMultiple } = require("../middlewares/multer");
+const auth = require("../middlewares/auth");
+
+router.get("/signin", adminController.viewSignin);
+router.post("/signin", adminController.actionSignin);
+router.use(auth);
+
+router.get("/dashboard", adminController.viewDashboard);
+router.get("/logout", adminController.actionLogout);
+
+// category routes
+router.get("/category", adminController.viewCategory);
+router.post("/category", adminController.addCategory);
+router.put("/category", adminController.editCategory);
+router.delete("/category/:id", adminController.deleteCategory);
+
+// bank routes
+router.get("/bank", adminController.viewBank);
+router.post("/bank", upload, adminController.addBank);
+router.put("/bank", upload, adminController.editBank);
+router.delete("/bank/:id", adminController.deleteBank);
+
+// item routes
+router.get("/item", adminController.viewItem);
+router.post("/item", uploadMultiple, adminController.addItem);
+router.get("/item/show-image/:id", adminController.showImageItem);
+router.get("/item/:id", adminController.showEditItem);
+router.put("/item/:id", uploadMultiple, adminController.editItem);
+router.delete("/item/:id/delete", adminController.deleteItem);
+
+// detail item - feature routes
+router.get("/item/show-detail-item/:itemId", adminController.viewDetailItem);
+router.post("/item/add/feature", upload, adminController.addFeature);
+router.put("/item/update/feature", upload, adminController.editFeature);
+router.delete("/item/:itemId/feature/:id", adminController.deleteFeature);
+
+// detail item - activity routes
+router.post("/item/add/activity", upload, adminController.addActivity);
+router.put("/item/update/activity", upload, adminController.editActivity);
+router.delete("/item/:itemId/activity/:id", adminController.deleteActivity);
+
+router.get("/booking", adminController.viewBooking);
+router.get("/booking/:id", adminController.showDetailBooking);
+router.put("/booking/:id/confirmation", adminController.actionConfirmation);
+router.put("/booking/:id/reject", adminController.actionReject);
+
+module.exports = router;
