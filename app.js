9c5215322febf4b9767f3b535e484628b9415c47
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-11-06 13:30:24 +0700

    allow cors

--- app.js
+++ app.js
@@ -50,6 +50,16 @@ app.use(
   express.static(path.join(__dirname, "node_modules/startbootstrap-sb-admin-2"))
 );
 
+// allow cors
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/admin", adminRouter);
