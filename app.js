f73599914d2c8616ede79ee2e64d09a2ea3d0a94
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-11-06 15:58:40 +0700

    allow cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require("http-errors");
 var express = require("express");
+var cors = require("cors");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -26,6 +27,9 @@ var apiRouter = require("./routes/api");
 
 var app = express();
 
+// allow cors
+app.use(cors());
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -49,16 +53,6 @@ app.use(
   "/sb-admin-2",
   express.static(path.join(__dirname, "node_modules/startbootstrap-sb-admin-2"))
 );
-
-// allow cors
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
