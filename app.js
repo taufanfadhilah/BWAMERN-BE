03eb3d92083d65f14d6941418936054304cf61dd
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-11-06 16:20:32 +0700

    fixing cors

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var adminRouter = require("./routes/admin");
 var apiRouter = require("./routes/api");
 
 var app = express();
-app.options("*", cors()); // include before other routes
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -42,6 +42,7 @@ app.use(
   })
 );
 app.use(flash());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
